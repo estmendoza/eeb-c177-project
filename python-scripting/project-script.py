ded87727264d8a7daa2be08116cb574533d0ff39
Esteban Mendoza <emendoza15331@gmail.com>
2020-03-01 19:59:07 -0800

    updated script to accept user input and played with regex

--- python-scripting/project-script.py
+++ python-scripting/project-script.py
@@ -21,55 +21,103 @@ def numpy(pandas_data):
 data = numpy(data)
 #input the data file into the function
 
+
+data = pandas.read_csv('final_data.csv')
+columns = list(data.columns)
+columns = columns[4:-1]
+
+#A = is X a disease (rank from 1-5)
+#B = should public funding be used for X (rank 1-5)
+import csv
+import re
+data = pandas.read_csv('final_data.csv')
+data = data.rename(columns={'Restless Legs SyndromeA.1':'Restless Legs SyndromeB', 'Personality DisorderA.1':'Personality DisorderB'})
+columns = list(data.columns)
+columns = columns[4:-1]
+
+regex = re.compile(r'[\w\s]*[^A]A{1}$')
+columnsA = list(filter(regex.match,columns))
+
+regex = re.compile(r'[\w\s]*[^B]B{1}$')
+columnsB = list(filter(regex.match,columns))
+
+
 dis = str(input("what disease do you want to compare?: "))
 #ask what disease to evaluate
-dataset = pandas.read_csv('final_data.csv')
-df = pandas.DataFrame(dataset)
-df = df[['Group', dis]]
-#extract data columns for people surveyed, and disease selected by user
+dis = dis.upper()
+dis = dis.split()
+error = False
+for word in dis:
+    if word in columnsA:
+        pass
+    else:
+        error = True
+if error: print('Please enter a valid condition')
+else:
+    def listtostring(s):
+        str1 = " "
+        return(str1.join(s))
+    dis = listtostring(dis)
+    dataset = pandas.read_csv('final_data.csv')
+    df = pandas.DataFrame(dataset)
+    df = df[['Group', dis]]
+    #extract data columns for people surveyed, and disease selected by user
 
-from collections import Counter
+    from collections import Counter
 
-classify = float(input('Percent of participants that considered {} as a disease on a scale from 1-5: '.format(dis)))
-#find percent of people who classified X as a disease on a scale from 1-5
-#choose a number on the scale
-def profession(data, person):
-    person_HArank = df.values.tolist() #make the people and classifications into a list
-    HA5_people = person_HArank.count([person, classify]) #count list items that include groups of people that ranked X condition as X rank
-    person_HArank = [tuple(i) for i in person_HArank] #tuple instead of list itmes
-    counts = Counter(x[0] for x in person_HArank) #count number of tuple items
-    total_people = counts[person] #count all people in the survey
-    percentage = HA5_people/total_people*100 #calculate percentage of people who ranked X condition as X rank
-    return percentage
-
-#different groups of people from data
-layperson = profession(data, 'Layperson')
-nurse = profession(data, 'Nurse')
-doctor = profession(data, 'Doctor')
-parliament = profession(data, 'Parliament')
-
-import matplotlib.pyplot as plt
-#%matplotlib inline
-plt.style.use('ggplot')
-#make a bar graph
-def plot_percentage_person(layperson, doctor, nurse, parliament):
-    #function to plot people and percentages
-    x = ['Layperson', 'Doctor','Nurse','Parliament'] #people on the x axis
-    percent = [layperson, doctor, nurse, parliament] #percentages to be calculated per person
-    x_pos = [i for i, _ in enumerate(x)] #add groups of people
-#bar graph settings
-    plt.bar(x_pos, percent, color='green')
-    plt.xlabel("Person")
-    plt.ylabel("Percent")
-    plt.title("Percent of professionals surveyed who classify {} \n as a rank {} disease on a scale of 1-5".format(dis, classify))
+    classify = str(input('Percent of participants that considered {} as a disease on a scale from 1-5: '.format(dis)))
+    #find percent of people who classified X as a disease on a scale from 1-5
+    #choose a number on the scale
+    error = False
+    number = [str(i) for i in range(1,6)]
+    #number = map(str, range(1, 6))
+    for rank in classify:
+        if rank in number:
+            pass
+        else:
+            error = True
+    if error: print('please enter a rank from 1-5')
+    else:
+        classify = float(classify)
 
 
+        def profession(data, person):
+            person_HArank = df.values.tolist() #make the people and classifications into a list
+            HA5_people = person_HArank.count([person, classify]) #count list items that include groups of people that ranked X condition as X rank
+            person_HArank = [tuple(i) for i in person_HArank] #tuple instead of list itmes
+            counts = Counter(x[0] for x in person_HArank) #count number of tuple items
+            total_people = counts[person] #count all people in the survey
+            percentage = HA5_people/total_people*100 #calculate percentage of people who ranked X condition as X rank
+            return percentage
 
-    plt.xticks(x_pos, x)
+        #different groups of people from data
+        layperson = profession(data, 'Layperson')
+        nurse = profession(data, 'Nurse')
+        doctor = profession(data, 'Doctor')
+        parliament = profession(data, 'Parliament')
+
+        import matplotlib.pyplot as plt
+        #%matplotlib inline
+        plt.style.use('ggplot')
+        #make a bar graph
+        def plot_percentage_person(layperson, doctor, nurse, parliament):
+            #function to plot people and percentages
+            x = ['Layperson', 'Doctor','Nurse','Parliament'] #people on the x axis
+            percent = [layperson, doctor, nurse, parliament] #percentages to be calculated per person
+            x_pos = [i for i, _ in enumerate(x)] #add groups of people
+        #bar graph settings
+            plt.bar(x_pos, percent, color='green')
+            plt.xlabel("Person")
+            plt.ylabel("Percent")
+            plt.title("Percent of professionals surveyed who classify {} \n as a rank {} disease on a scale of 1-5".format(dis, classify))
 
 
-#plot the graph
-    plt.show()
-    return
-plot_percentage_person(layperson, doctor, nurse, parliament)
-#use the function
+
+            plt.xticks(x_pos, x)
+
+
+        #plot the graph
+            plt.show()
+            return
+        plot_percentage_person(layperson, doctor, nurse, parliament)
+        #use the function
